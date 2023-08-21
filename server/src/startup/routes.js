6850b0bf66abe09f3c2d85b83295b9814c8db556
carlosaj98 <carlosaj.arahal@gmail.com>
2023-08-21 18:16:31 +0200

    feat: helmet and compression dependences

--- server/src/startup/routes.js
+++ server/src/startup/routes.js
@@ -1,20 +1,21 @@
 require('express-async-errors')
 const { json } = require('express')
 const morgan = require('morgan')
-// const errors = require('../middlewares/errors')
+const helmet = require("helmet")
+const compression = require("compression")
+const errors = require('../middlewares/errors')
 
 module.exports = function (app) {
 	app.use(json())
 	app.use(morgan('tiny'))
 
-	// app.use(helmet())
-	// app.use(compression())
+	app.use(helmet())
+	app.use(compression())
 
 	app.use('/api/users', require('../routes/usersRoutes'))
 	app.use('/api/suggestions', require('../routes/suggestionsRoutes'))
 	app.use('/api/suggestions', require('../routes/commentsRoutes'))
-	// app.use('/api/scores', require('../routes/scoreRoutes'))
-	// app.use(errors)
+	app.use(errors)
 }
 
 // Repaso manejo de errores
