42e2a66caa15940e9c40389c5283c20bd3ff961d
carlosaj98 <carlosaj.arahal@gmail.com>
2023-08-29 13:52:43 +0200

    update: Navbar style

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import RoadmapPreview from "../RoadmapPreview"
+
 import Styled from "./Style"
+
 
 function Navbar() {
   return (
@@ -16,7 +17,29 @@ function Navbar() {
           <Styled.CategoryButton>Bug</Styled.CategoryButton>
           <Styled.CategoryButton>Feature</Styled.CategoryButton>
       </div>
-      <RoadmapPreview />
+      <div id="roadmap-preview">
+      <div id="rm-header">
+        <p>Roadmap</p>
+        <a href="">View</a>
+      </div>
+      <ul id="rm-list">
+        <li className="rm-list-item">
+          <div id="dot-planned"></div>
+          <p className="rm-item-text">Planned</p>
+          <p className="rm-item-number">0</p>
+        </li>
+        <li className="rm-list-item">
+          <div id="dot-inprogress"></div>
+          <p className="rm-item-text">In-Progress</p>
+          <p className="rm-item-number">0</p>
+        </li>
+        <li className="rm-list-item">
+          <div id="dot-live"></div>
+          <p className="rm-item-text">Live</p>
+          <p className="rm-item-number">0</p>
+        </li>
+      </ul>
+    </div>
     </Styled.Navbar>
   )
 }
--- client/src/components/Navbar/Style.jsx
+++ client/src/components/Navbar/Style.jsx
@@ -40,6 +40,80 @@ const Navbar = styled("nav")({
         padding: "24px",
         borderRadius: "10px",
     },
+
+    "#roadmap-preview":{
+        width: "100%",
+        height: "178px",
+        backgroundColor:"var(--white)",
+        display: "flex",
+        flexDirection:"column",
+        padding:"24px 19px 24px 24px",
+        borderRadius: "10px"
+    },
+
+    "#rm-header":{
+        display: "flex",
+        justifyContent:"space-between",
+        marginBottom: "24px",
+        alignItems: "baseline",
+    },
+
+    "#rm-header p":{
+        color: "var(--dark-blue-text)",
+        fontSize: "18px",
+        fontWeight: "800",
+    },
+
+    "#rm-header a":{
+        color: "var(--blue)",
+        fontSize: "13px",
+        fontWeight: "600",
+        "&:active":{
+            color:"#8397F8"
+        }
+    },
+
+    "#rm-list":{
+        display: "flex",
+        flexDirection:"column",
+        listStyle:"none"
+    },
+
+    ".rm-list-item":{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "baseline",
+        fontSize: "16px",
+        gap: "16px",
+        color: "var(--gray)"
+    },
+    "#dot-planned":{
+        width: "8px",
+        height: "8px",
+        backgroundColor: "var(--orange)",
+        borderRadius:"100%",
+    },
+    "#dot-inprogress":{
+        width: "8px",
+        height: "8px",
+        backgroundColor: "var(--purple)",
+        borderRadius:"100%",
+    },
+    "#dot-live":{
+        width: "8px",
+        height: "8px",
+        backgroundColor: "var(--light-blue)",
+        borderRadius:"100%",
+    },
+    ".rm-item-text":{
+        flexGrow: "2",
+
+    },
+
+    ".rm-item-number":{
+        fontWeight:"700"
+    }
+
 })
 
 const CategoryButton = styled(Button)({
