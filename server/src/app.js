78f5a300d5d904dd7ec7345fd5bcae7e1879464d
carlosaj98 <carlosaj.arahal@gmail.com>
2023-11-19 20:18:58 +0100

    feat: health check endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -8,4 +8,6 @@ require("./startup/config")()
 require("./startup/bd")()
 require("./startup/routes")(app)
 
+app.get("/ping", (req, res) => res.send("pong"))
+
 app.listen(port, () => console.log("SERVER ON PORT... " + port))
